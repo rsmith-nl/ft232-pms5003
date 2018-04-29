98aba92d312de623473c3c6999076c225d6a7785
Roland Smith <rsmith@xs4all.nl>
2018-04-29 15:05:11 +0200

    Merge improvements from master.

--- dust-monitor.py
+++ dust-monitor.py
@@ -5,24 +5,37 @@
 # Copyright © 2018 R.F. Smith <rsmith@xs4all.nl>.
 # SPDX-License-Identifier: MIT
 # Created: 2018-04-11T18:52:43 +0200
-# Last modified: 2018-04-27T13:45:45+0200
+# Last modified: 2018-04-29T15:04:10+0200
 """
 Monitoring program for the plantower PMS5003 air monitoring sensor.
 The sensor is connected to the computer via an FT232H, used as a serial to
 USB converter.
+
+Connect the 5V from the FT232H to VCC on the PMS5003. Conncect GND to GND.
+Connect D0 to RXD and D1 to TXD. This program does not use the SET or RST pins.
 
 This runs the sensor in passive mode. Note that for passive mode,
 both TX and RX must be connected! It logs the data to a file.
 """
 
 from datetime import datetime
+from enum import Enum
 import argparse
 import struct
 import sys
 import time
 import serial
 
-__version__ = '1.0'
+__version__ = '1.1'
+
+
+# See Appendix II (page 15) of the (annotated) PMS5003 manual.
+class Cmd(Enum):
+    PASSIVE_READ = b'\x42\x4d\xe2\x00\x00\x01\x71'
+    PASSIVE_MODE = b'\x42\x4d\xe1\x00\x00\x01\x70'
+    ACTIVE_MODE = b'\x42\x4d\xe1\x00\x01\x01\x71'
+    SLEEP = b'\x42\x4d\xe4\x00\x00\x01\x73'
+    WAKEUP = b'\x42\x4d\xe4\x00\x01\x01\x74'
 
 
 def main(argv):
@@ -36,11 +49,11 @@ def main(argv):
     args = process_arguments(argv)
 
     # Open the files
-    ft232h = serial.Serial(args.port, 9600, timeout=1)
+    ft232h = serial.Serial(args.device, 9600, timeout=1)
     datafile = open(args.path.format(now), 'w')
 
     # Set the sensor to passive mode.
-    ft232h.write(b'\x42\x4d\xe1\x00\x00\x01\x70')
+    ft232h.write(Cmd.PASSIVE_MODE)
     # Drop all exesting active mode data.
     ft232h.flushInput()
 
@@ -65,7 +78,7 @@ def main(argv):
     try:
         while True:
             # Request data
-            ft232h.write(b'\x42\x4d\xe2\x00\x00\x01\x71')
+            ft232h.write(Cmd.PASSIVE_READ)
             # Read data
             data = ft232h.read(32)
             now = datetime.utcnow().strftime('%FT%TZ ')
@@ -102,17 +115,17 @@ def main(argv):
 def process_arguments(argv):
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
-        '-p',
-        '--port',
+        '-d',
+        '--device',
         default='/dev/cuaU0',
         type=str,
-        help='serial port to use (default /dev/cuaU0)')
+        help='serial device to use (default "/dev/cuaU0")')
     parser.add_argument(
         '-i',
         '--interval',
         default=5,
         type=int,
-        help='interval between measurements (≥5 s, default 5 s)')
+        help='interval between measurements in seconds (≥5 s, default 5 s)')
     parser.add_argument(
         '-v', '--version', action='version', version=__version__)
     parser.add_argument(
